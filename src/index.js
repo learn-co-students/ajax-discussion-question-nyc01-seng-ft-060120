1974d70fccb908337e92ae9a935fb7fec2fdf58e
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-20 10:31:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  
+  const button = document.querySelector('.btn');
+  //console.log(button);
+  button.addEventListener('click', function(e) => {
+    
+  })
 });
