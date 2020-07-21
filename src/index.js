1f6750f550c2bc53e8ae864f0637ea0e5ba3f773
travisprol0 <travisjohnprol@gmail.com>
2020-07-21 08:30:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  .then(results => {
+    renderPerson(results.results[0]);
+  });
+
+  function renderPerson(person){
+    const container = document.querySelector(".container")
+    const img = document.createElement("img")
+    img.src = person.picture.large
+    container.appendChild(img)
+  }
 });
